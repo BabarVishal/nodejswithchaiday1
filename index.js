9ec4d529f8b1f15a2d4337ea9fda80fc7504d6b2
vishal <vishalbabar7351@gmail.com>
2024-04-14 20:18:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+
+ const port = 4000;
+ 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get("/About", (req, res) =>{
+    res.send("hy Vishal");
+})
+
+app.listen(process.env.port,() => {
+  console.log(`Example app listening on port ${port}`)
+})
